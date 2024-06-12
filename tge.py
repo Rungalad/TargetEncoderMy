f32643c2a69a722e3b6a86295f85ad37c91113ef
Rungalad <52277857+Rungalad@users.noreply.github.com>
2024-06-12 18:10:10 +0300

    fix no existing values in test dataset

--- tge.py
+++ tge.py
@@ -1,10 +1,14 @@
+import numpy as np
+import pandas as pd
+
 class TGE:
 
     def __init__(self, fluctuate=0.1):
         self.columns_params = dict()
         self.fluctuate = fluctuate
 
-    def fit(self, data: pd.DataFrame, y: np.ndarray) -> None:
+    def fit(self, data_: pd.DataFrame, y: np.ndarray) -> None:
+        data = copy.deepcopy(data_)
         data['target'] = y
         di = {}
         for col in data.columns:
@@ -29,9 +33,17 @@ class TGE:
     def transform(self, data: pd.DataFrame) -> pd.DataFrame:
         output = copy.deepcopy(data)
         for col in self.columns_params:
-            output[col] = output[col].apply(lambda x: self.columns_params[col][x])
+            values = output[col].values
+            remade_values = []
+            mean_val = np.mean(list(self.columns_params[col].values()))
+            for elem in values:
+                if elem in self.columns_params[col]:
+                    remade_values.append(self.columns_params[col][elem])
+                else:
+                    remade_values.append(mean_val)
+            output[col] = remade_values
         return output
 
 e = TGE(fluctuate=0.1)
-cat_cols = [i for i in X.columns if X[i].dtypes == 'object']
-cat_feats = e.fit_transform(X[cat_cols], y)
+cat_cols = [i for i in X_train.columns if X_train[i].dtypes == 'object']
+cat_feats = e.fit_transform(X_train[cat_cols], y_train)
